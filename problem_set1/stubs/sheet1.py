8aaf669467df4d922664168addd8fae2d2a2757b
raskade <alexanderschmidt2111@gmail.com>
2024-05-04 23:22:01 +0200

    delete Assignment4.py create sheet1.py; update lle to show if graph is connected

--- problem_set1/stubs/sheet1.py
+++ problem_set1/stubs/sheet1.py
@@ -2,16 +2,18 @@ import numpy as np
 from scipy.spatial import distance_matrix
 
 
-def lle(X, m, n_rule, param, tol=1e-2):
+#assignment 4
+def lle(X, m, n_rule, k=None, epsilon = None, tol=1e-2):
     n = X.shape[0]
     D = distance_matrix(X, X)
     if n_rule == 'knn':
-        neighbors = np.argsort(D, axis=1)[:, 1:param + 1]
+        neighbors = np.argsort(D, axis=1)[:, 1:k + 1]
     elif n_rule == 'eps-ball':
-        neighbors = [np.where(D[i] <= param)[0] for i in range(n)]
+        neighbors = [np.where(D[i] <= epsilon)[0] for i in range(n)]
     else:
         raise ValueError("Invalid neighborhood rule specified.")
-
+    if k == 0 or not all(len(x)>1 for x in neighbors):
+        raise ValueError("Graph is not connected")
     w_initial = np.zeros((n, n))
     for i in range(n):
         k_i = neighbors[i]
@@ -23,5 +25,5 @@ def lle(X, m, n_rule, param, tol=1e-2):
     M = np.eye(n) - w_initial
     M = np.dot(M.T, M)
     eigenvalues, eigenvectors = np.linalg.eigh(M)
-    idx = np.argsort(eigenvalues)[1:m + 1]  # ignore the first smallest zero eigenvalue
+    idx = np.argsort(eigenvalues)[1:m + 1]
     return eigenvectors[:, idx]
